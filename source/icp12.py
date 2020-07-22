bb307e0e90042468900fcf4a6846f24f4fe1a7c5
sowmyareddy14 <60200076+sowmyareddy14@users.noreply.github.com>
2020-07-21 23:10:21 -0500

    Update icp12.py

--- source/icp12.py
+++ source/icp12.py
@@ -1,70 +1,54 @@
-#--- Importing the libraries
 from pyspark import SparkContext,SQLContext
-import os
-os.environ["PYSPARK_SUBMIT_ARGS"] = ("--packages  graphframes:graphframes:0.8.0-spark2.4-s_2.11 pyspark-shell")
-
 from graphframes import *
-from pyspark.sql import functions as f
-from pyspark.sql.functions import col, lit, when, concat, desc
+import pyspark.sql.functions as f
 
-#---1.Importing the dataset as a csv file and creating data frames directly on import--
 sc = SparkContext.getOrCreate()
+sc.addPyFile('C:\spark-3.0.0-preview2-bin-hadoop2.7\jars\graphframes-0.8.0-spark3.0-s_2.12.jar')
 sqlcontext=SQLContext(sc)
 
-station_dataf= sqlcontext.read.format("csv").option("header", "true").csv('201508_station_data.csv')
-trips_dataf= sqlcontext.read.format("csv").option("header", "true").csv('201508_trip_data.csv')
-station_dataf.show()
+#question 1
+station_data_df= sqlcontext.read.format("csv").option("header", "true").csv(r'C:\Users\jagad\Downloads\201508_station_data.csv')
+trips_df= sqlcontext.read.format("csv").option("header", "true").csv(r'C:\Users\jagad\Downloads\201508_trip_data.csv')
+station_data_df.show()
 
-#---2.Concatenate the chunks into list & convert to DataFrame---
-concat = station_dataf.select(concat("lat", lit(","),"long")).alias("location")
+#question 2 - lit is used to interact with column literals in pyspark
+concat = station_data_df.select(f.concat("lat",f.lit("=>"),"long"))
 concat.show()
 
-#-----3.Remove duplicates-------
-station_dataf_dropduplicates =  station_dataf.dropDuplicates()
-station_dataf_dropduplicates.show()
+#question 3
+station_data_df_dropduplicates =  station_data_df.dropDuplicates()
+station_data_df_dropduplicates.show()
 
-#-----4.Name Columns--------
-station_data_df_renamecolumns=station_dataf.withColumnRenamed("name","Station_name").withColumnRenamed("installation","installation_date")
-
-#------5.Output DataFrame-----
+#question 4 and 5
+station_data_df_renamecolumns=station_data_df.withColumnRenamed("station_id","id").select("id").distinct()
 station_data_df_renamecolumns.show()
 
-#------6.Create vertices------
-vertices = station_dataf.withColumnRenamed("name","id").distinct()
-vertices.show()
+#question 6
+station_vertices = station_data_df.withColumnRenamed("name","id").select("id").distinct()
+station_vertices.show()
 
-trip_edges=trips_dataf.withColumnRenamed("Start Station","src").withColumnRenamed("End Station","dst")
+trip_edges=trips_df.withColumnRenamed("Start Station","src")\
+.withColumnRenamed("End Station","dst")\
+.select("src","dst")
+
+g=GraphFrame(station_vertices,trip_edges)
 
 
-#------create graph out of the data frame created.
-g = GraphFrame(vertices,trip_edges)
-print(g)
-
-#-----7.Show some vertices----
+#Question 7
 g.vertices.show()
 
-#-----8.Show some edges-------
+#question 8
 g.edges.show()
 
-#-----9.Vertex in-Degree------
+#question 9
 g.inDegrees.show()
 
-#-----10.Vertex out-Degree-----
+#question 10
 g.outDegrees.show()
 
-#-----11.Apply the motif findings-----
-motifs = g.find("(a)-[e]->(b); (b)-[e2]->(a)")
-motifs.show()
+#question 11
+g.find("(a)-[b]->(e)").show() #search verices connected by edges
 
-#-----12.Stateful Queries--------
-# Get Edges with duration greater than 500
-g.edges.filter("duration > 764").sort("duration").show()
-
-# Subgraphs
-subgraph_vertices = g.vertices.filter("dockcount > 25")
-subgraph_edges = g.edges.filter("duration > 500")
-subgraph = GraphFrame(subgraph_vertices,subgraph_edges)
-subgraph.vertices.show()
 
 #Bonus
 #-----1.Vertex degree---------
